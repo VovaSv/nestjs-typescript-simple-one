8735342cdb90c3139615105662b8075c21e8a8fe
VovaSv <vovasvidi@gmail.com>
2023-05-17 15:33:05 +0300

    DB Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // Without passing nothing the ConfigModule will auto use .env file from root
+    // ConfigModule.forRoot({ isGlobal: true }) -> to remove import in top-page.module
+    MongooseModule.forRoot(
+      'mongodb+srv://vovasvidi:OxUzvm6arepupnpo@cluster69.0nfk51j.mongodb.net/?retryWrites=true&w=majority&ssl=true',
+    ),
     AuthModule,
     TopPageModule,
     ProductModule,
